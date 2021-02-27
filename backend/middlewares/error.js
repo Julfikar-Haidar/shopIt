9567b62049871aed1334283cfb03a13153ba0a0e
Julfikar Haidar <julfikar6262@gmail.com>
2021-02-28 00:28:02 +0600

    proper handeling authentication

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -17,6 +17,27 @@ module.exports =(err, req, res, next) => {
         const message = Object.values(err.errors).map(value => value.message)
          err = new ErrorHandler(message, 400)
     }
+
+     // Handling Mongoose duplicate key errors
+     if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        err = new ErrorHandler(message, 400)
+    }
+
+    // Handling wrong JWT error
+    if (err.name === 'JsonWebTokenError') {
+        const message = 'JSON Web Token is invalid. Try Again!!!'
+        err = new ErrorHandler(message, 400)
+    }
+
+    // Handling Expired JWT error
+    if (err.name === 'TokenExpiredError') {
+        const message = 'JSON Web Token is expired. Try Again!!!'
+        err = new ErrorHandler(message, 400)
+    }
+
+
+
         res.status(err.statusCode).json({
             success: false,
             // error: err,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 
-const { registerUser, loginUser, logout } = require('../controller/authController')
+const { registerUser, loginUser, logout, forgotPassword , resetPassword } = require('../controller/authController')
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser);
 router.route('/logout').get(logout);
-
+router.route('/password/forgot').post(forgotPassword);
+router.route('/password/reset/:token').put(resetPassword);
 
 
 module.exports = router
